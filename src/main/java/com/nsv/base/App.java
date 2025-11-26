ea4fad647926c0e88d6534939a6f98631fb1b343
NosovArtem <nosovartem1992@gmail.com>
2025-11-26 11:41:54 +0300

    simple mirror bot

--- src/main/java/com/nsv/base/App.java
+++ src/main/java/com/nsv/base/App.java
@@ -1,10 +1,30 @@
 package com.nsv.base;
 
+
+import io.github.cdimascio.dotenv.Dotenv;
+import lombok.extern.slf4j.Slf4j;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
 /**
  * Hello world!
  */
+@Slf4j
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws TelegramApiException {
+        Dotenv dotenv = Dotenv.load(); // TODO:
+        String bot_token = dotenv.get("BOT_TOKEN");
+        String bot_name = dotenv.get("BOT_NAME");
+
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        try {
+            telegramBotsApi.registerBot(new Bot(bot_name, bot_token));
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
---
+++ src/main/java/com/nsv/base/Bot.java
@@ -0,0 +1,65 @@
+package com.nsv.base;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+
+
+@Slf4j
+@AllArgsConstructor
+public class Bot extends TelegramLongPollingBot {
+    @Getter
+    String userName;
+    @Getter
+    String token;
+
+    /**
+     * Метод для приема сообщений.
+     * @param update Содержит сообщение от пользователя.
+     */
+    @Override
+    public void onUpdateReceived(Update update) {
+        String message = update.getMessage().getText();
+        sendMsg(update.getMessage().getChatId().toString(), message);
+    }
+
+    /**
+     * Метод для настройки сообщения и его отправки.
+     * @param chatId id чата
+     * @param s Строка, которую необходимот отправить в качестве сообщения.
+     */
+    public synchronized void sendMsg(String chatId, String s) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(s);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+
+    /**
+     * Метод возвращает имя бота, указанное при регистрации.
+     * @return имя бота
+     */
+    @Override
+    public String getBotUsername() {
+        return userName;
+    }
+
+    /**
+     * Метод возвращает token бота для связи с сервером Telegram
+     * @return token для бота
+     */
+    @Override
+    public String getBotToken() {
+        return token;
+    }
+}
